7f0d4628452b00f68a8fea0347bf07837e9bbb2c
Miller <gelaseen@gmail.com>
2021-12-02 03:46:23 +0700

    Initial Commit

---
+++ PursuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PursuApp
+//
+//  Created by Robert Miller on 02.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
