1f89f6bce43290401846e0ab45074e7687127bfb
Miller <gelaseen@gmail.com>
2022-01-11 22:03:14 +0700

    Koloda swiping setup

---
+++ PursuApp/CardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CardViewController.swift
+//  PursuApp
+//
+//  Created by Robert Miller on 11.01.2022.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- PursuApp/ViewController.swift
+++ PursuApp/ViewController.swift
@@ -9,12 +9,56 @@ import UIKit
 import Koloda
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var kolodaView: KolodaView!
+    
+    let activityCount = 5
+    var containers = [CardViewController]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadContainerView()
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadContainerView()
+        kolodaView.reloadData()
+    }
+    
+    func loadContainerView(){
+        for _ in 0...activityCount - 1{
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "CardViewController") as! CardViewController
+            self.addChild(vc)
+            containers.append(vc)
+        }
+    }
+    
+    
 }
 
+extension ViewController: KolodaViewDataSource {
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        let container = containers[index]
+        return container.view
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        activityCount
+    }
+    
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        .default
+    }
+    
+    
+}
+
+extension ViewController: KolodaViewDelegate {
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+           koloda.reloadData()
+       }
+
+}
